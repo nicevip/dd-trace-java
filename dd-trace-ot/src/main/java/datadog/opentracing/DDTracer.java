9470d1a87f2cfd67b086b4f0fcedb8b77af5982e
Tyler Benson <tyler.benson@datadoghq.com>
2019-01-02 16:25:32 -0800

    Minimal changes required for OT 0.32
    This seems backwards compatable with 0.31 since all changes are additional, so this should be safe.
    
    USING RC.  DON’T MERGE.

--- dd-trace-ot/src/main/java/datadog/opentracing/DDTracer.java
+++ dd-trace-ot/src/main/java/datadog/opentracing/DDTracer.java
@@ -24,6 +24,7 @@ import io.opentracing.ScopeManager;
 import io.opentracing.Span;
 import io.opentracing.SpanContext;
 import io.opentracing.propagation.Format;
+import io.opentracing.tag.Tag;
 import java.io.Closeable;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -298,6 +299,11 @@ public class DDTracer implements io.opentracing.Tracer, Closeable, datadog.trace
   }
 
   @Override
+  public Scope activateSpan(final Span span) {
+    return scopeManager.activate(span);
+  }
+
+  @Override
   public DDSpanBuilder buildSpan(final String operationName) {
     return new DDSpanBuilder(operationName, scopeManager);
   }
@@ -384,8 +390,8 @@ public class DDTracer implements io.opentracing.Tracer, Closeable, datadog.trace
   }
 
   @Override
-  public void addScopeListener(ScopeListener listener) {
-    this.scopeManager.addScopeListener(listener);
+  public void addScopeListener(final ScopeListener listener) {
+    scopeManager.addScopeListener(listener);
   }
 
   @Override
@@ -492,6 +498,11 @@ public class DDTracer implements io.opentracing.Tracer, Closeable, datadog.trace
     @Override
     public DDSpanBuilder withTag(final String tag, final boolean bool) {
       return withTag(tag, (Object) bool);
+    }
+
+    @Override
+    public <T> SpanBuilder withTag(final Tag<T> tag, final T value) {
+      return withTag(tag.getKey(), value);
     }
 
     @Override
